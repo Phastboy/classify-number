0835d1251ff20eb311301e403d49cfd0453631de
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-03 10:18:44 +0100

    feat(api): enhance Swagger documentation for classify-number endpoint
    - Replaced @ApiResponse with @ApiOkResponse and @ApiBadRequestResponse for better clarity.
    
    - Improved formatting for better readability and consistency.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Logger, BadRequestException } from '@nestjs/common';
-import { ApiTags, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import {
+    ApiTags,
+    ApiQuery,
+    ApiOkResponse,
+    ApiBadRequestResponse,
+} from '@nestjs/swagger';
 import { ClassifyNumberResponseDto } from './dto/classify-number-response.dto';
 import { BadRequestResponseDto } from './dto/bad-request-response.dto';
 
@@ -13,8 +18,14 @@ export class AppController {
 
     @Get('classify-number')
     @ApiQuery({ name: 'number', required: true, type: String })
-    @ApiResponse({ status: 200, description: 'Successful response', type: ClassifyNumberResponseDto })
-    @ApiResponse({ status: 400, description: 'Invalid number parameter', type: BadRequestResponseDto })
+    @ApiOkResponse({
+        description: 'Successful response',
+        type: ClassifyNumberResponseDto,
+    })
+    @ApiBadRequestResponse({
+        description: 'Invalid number parameter',
+        type: BadRequestResponseDto,
+    })
     async classify(@Query('number') number: string) {
         const parsedNumber = parseInt(number, 10);
         if (isNaN(parsedNumber)) {
